de6baacb5b72d25016ba6e3e4d3e637f909ea427
Juan Jose Romero Montes <claseinfojuanjose@gmail.com>
2024-06-12 13:48:14 +0200

    Fix: swippers have unique id

--- scripts/Swiper.js
+++ scripts/Swiper.js
@@ -1,16 +1,37 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
+const swiperRooms = new Swiper('#swiper-rooms', {
     direction: 'horizontal',
     loop: true,
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
   
-    // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '#swiper-button-next-rooms',
+      prevEl: '#swiper-button-prev-rooms',
     }
+});
+
+const swiperFeatures = new Swiper('#swiper-features', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '#swiper-pagination-features',
+  }
+});
+
+const swiperFood = new Swiper('#swiper-food', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '#swiper-button-next-food',
+    prevEl: '#swiper-button-prev-food',
+  }
+});
+
+const swiperCarousel = new Swiper('#swiper-carousel', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '#swiper-pagination-carousel',
+  }
 });
